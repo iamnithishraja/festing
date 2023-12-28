9e5deb72df35363852ec9a6a776b0494699647eb
nithish932 <nithishraja248@gmail.com>
2023-12-29 01:40:43 +0530

    production cyclic

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,18 @@
 import dotenv from "dotenv";
-dotenv.config({path:"config/.env"})
+dotenv.config({ path: "config/.env" })
 import mongoose from "mongoose";
 
 function connectDatabse() {
-    mongoose.connect(process.env.DB_URI).then(
-        () => { console.log("connected succesfully") }
-    ).catch(err => {
-        console.log(err);
-    });   
+    return new Promise((resolve) => {
+        return mongoose.connect(process.env.DB_URI).then(
+            () => {
+                console.log("connected succesfully");
+                resolve();
+            }
+        ).catch(err => {
+            console.log(err);
+        });
+    });
 }
 
-export {connectDatabse};
+export { connectDatabse };
