dd261e2c54609455ef420b621b094ebe5b408660
iamnithishraja <153272518+iamnithishraja@users.noreply.github.com>
2024-03-13 11:50:14 +0530

    version upgrade cr registration feature

--- backend/frontend/flutter_service_worker.js
+++ backend/frontend/flutter_service_worker.js
@@ -32,11 +32,11 @@ const RESOURCES = {"assets/AssetManifest.bin": "7b7fd69023555614b615b867f500064b
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8713e191d30fe27ecf7470b9d16bf387",
-"/": "8713e191d30fe27ecf7470b9d16bf387",
+"index.html": "f9fbbc4cd4caf51bfbd1002bea293acd",
+"/": "f9fbbc4cd4caf51bfbd1002bea293acd",
 "main.dart.js": "d95dc6427a6061c9933dd79cc5e177af",
 "manifest.json": "a147abe7f2a49abb97cf55cf6c0af53e",
-"version.json": "c7978d514249528a3a21cb401aa17081"};
+"version.json": "f1d1bba7b5b6184747b9be93a7f0758d"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
